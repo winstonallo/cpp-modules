acccb15c9bb9f01057e6704b6ea47260b6f65d60
winstonallo <arthurbiedchar@gmail.com>
2024-01-04 11:21:08 +0100

    adde3d commments cause i forget shit

--- cpp02/ex00/inc/Fixed.hpp
+++ cpp02/ex00/inc/Fixed.hpp
@@ -2,18 +2,25 @@
 #define FIXED_HPP
 
 #include <iostream>
-#include <type_traits>
 
 class Fixed{
     public:
+		//default constructor
         Fixed();
+		//destructor
         ~Fixed();
+		//copy constructor: creates a new object as a copy of an existing object
         Fixed(Fixed const & src);
-        Fixed&operator=(Fixed const&rhs);
-        int getRawBits()const;
+		//assignation operator: called when '=' is used between two objects
+        Fixed & operator = (Fixed const&rhs);
+		//getter function
+        int getRawBits() const;
+		//setter function
         void setRawBits(int const raw);
 	private:
+		//fixed point value
 		int	fixedPointValue;
+		//number of fractional bits (number of bits used to represent the fractional part of a number)
 		static const int nFractBits=8;
 };
 
--- cpp02/ex00/src/Fixed.cpp
+++ cpp02/ex00/src/Fixed.cpp
@@ -18,11 +18,16 @@ Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
 }
 
+/* copy constructor: calling this constructor with an object of the same class 
+as argument copies all the data members of the argument into the object being constructed */
 Fixed::Fixed(Fixed const&src){
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
+/* assignation operator: called when '=' is used between two objects
+to set the data members of the objedct being assigned to to the same values as the ones
+from the source*/
 Fixed&Fixed::operator=(Fixed const&rhs){
 	std::cout << "Copy assignment operator called" << std::endl;
 	setRawBits(rhs.getRawBits());
